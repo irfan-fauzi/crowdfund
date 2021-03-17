1ec2180e8f829df9db3380af79f34f9c19f36783
irfan-fauzi <irfanwintech@yahoo.com>
2021-03-17 18:39:44 +0700

    j

--- src/data/index.js
+++ src/data/index.js
@@ -37,30 +37,36 @@ export const numData = [
 ];
 
 export const dataAbout = {
-  id : 1,
-  title : "About this project",
-  caption1 : "The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform that elevates your screen to a more comfortable viewing height. Placing your monitor at eye level has the potential to improve your posture and make you more comfortable while at work, helping you stay focused on the task at hand.",
-  caption2 : "Featuring artisan craftsmanship, the simplicity of design creates extra desk space below your computer to allow notepads, pens, and USB sticks to be stored under the stand."
+  id: 1,
+  title: "About this project",
+  caption1: "The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform that elevates your screen to a more comfortable viewing height. Placing your monitor at eye level has the potential to improve your posture and make you more comfortable while at work, helping you stay focused on the task at hand.",
+  caption2: "Featuring artisan craftsmanship, the simplicity of design creates extra desk space below your computer to allow notepads, pens, and USB sticks to be stored under the stand."
 }
 
 export const dataCard = [
   {
-    id : 1,
-    title : "Bamboo Stand",
-    title2 : "Pledge $25 or more",
-    caption : "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and  you’ll be added to a special Backer member list. 101 left Select Reward"
+    id: 1,
+    title: "Bamboo Stand",
+    title2: "Pledge $25 or more",
+    caption: "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and  you’ll be added to a special Backer member list. 101 left Select Reward",
+    number: 101
+
   },
   {
-    id : 2,
-    title : "Black Edition Stand",
-    title2 : "Pledge $75 or more",
-    caption : "You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included. 64 left Select Reward"
+    id: 2,
+    title: "Black Edition Stand",
+    title2: "Pledge $75 or more",
+    caption: "You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included. 64 left Select Reward",
+    number: 64
+
   },
   {
-    id : 3,
-    title : "Mahogany Special Edition",
-    title2 : "Pledge $200 or more",
-    caption : "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added  to our Backer member list. Shipping is included. 0 left Out of Stock"
+    id: 3,
+    title: "Mahogany Special Edition",
+    title2: "Pledge $200 or more",
+    caption: "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added  to our Backer member list. Shipping is included. 0 left Out of Stock",
+    number: 0
+
   }
 ]
 
--- src/showcase/About.js
+++ src/showcase/About.js
@@ -1,12 +1,27 @@
 import React from 'react'
-import { dataAbout } from '../data/index';
+import { dataAbout, dataCard } from '../data/index';
 
 const About = () => {
   return (
     <div className="about">
       <h1>{dataAbout.title}</h1>
-      <p>{dataAbout.caption1}</p>
+      <p className="paragraf1">{dataAbout.caption1}</p>
       <p>{dataAbout.caption2}</p>
+      {
+        dataCard.map(el => (
+          <div key={Math.random()} className="card non-active">
+            <div className="text-wrap">
+              <h2>{el.title}</h2>
+              <h4>{el.title2}</h4>
+            </div>
+            <p>{el.caption}</p>
+            <div className="btn-wrap">
+              <h2 className="num">{el.number}<span>left</span></h2>
+              <button className="btn">Select Reward</button>
+            </div>
+          </div>
+        ))
+      }
     </div>
   )
 }
